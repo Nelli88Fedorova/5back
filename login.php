4e369cbf1d29d78b14444dbc908d7d8c116871d7
Нелли Федорова <90643474+Nelli88Fedorova@users.noreply.github.com>
2022-04-30 16:50:29 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,13 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 session_start();
-$msg="";//для сообщений
+$msg; //для сообщений
 
 // В суперглобальном массиве $_SESSION хранятся переменные сессии.
 // Будем сохранять туда логин после успешной авторизации.
-if (!empty($_SESSION['login'])) 
-{
-  // +++Если есть логин в сессии, то пользователь уже авторизован.
-  // ++++TODO: Сделать выход (окончание сессии вызовом session_destroy()
-  //при нажатии на кнопку Выход).
+if (!empty($_SESSION['login'])) {
+  // Если есть логин в сессии, то пользователь уже авторизован.
+  //   выход  session_destroy() при нажатии на кнопку Выход.
   printf('Пользователь с логином %s уже авторизован.', $_SESSION['login']);
   if (isset($_POST['exit'])) session_destroy();
   // Делаем перенаправление на форму.
@@ -18,56 +16,61 @@ if (!empty($_SESSION['login']))
 
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
-if ($_SERVER['REQUEST_METHOD'] == 'GET') 
-{
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
 
-<form action="" method="post">
-<label> login:<br />
-  <input name="login" value="<?php if(isset($loginuser)) print $loginuser; ?>"/>
-  </label><br />
+  <form action="" method="post">
+    <label> login:<br />
+      <!-- _____________________________________________4)________Заполняем пароль и логин из COOKIE___________________________ -->
+      <input name="login" value="<?php if (isset($_COOKIE['login'])) print $_COOKIE['login']; ?>" />
+    </label><br />
 
-  <label> password:<br />
-  <input name="pass" value="<?php if(isset($passuser)) print $passuser; ?>"/>
-  </label><br />
+    <label> password:<br />
+      <input name="pass" value="<?php if (isset($_COOKIE['pass'])) print $_COOKIE['pass']; ?>" />
+    </label><br />
 
-  <input type="submit" value="Войти" />
-  <input name="exit" type="submit" value="Выход" />
-</form>
+    <input name="enter" type="submit" value="Войти" />
+    <input name="registration" type="submit" value="регистрация" />
+    <input name="exit" type="submit" value="Выход" />
+  </form>
+
+  <?php
+  $msgname = array('update', 'notexist');
+  foreach ($msgname as $n)
+    if (isset($msg[$n])) print($msg[$n]);
+  ?>
 
 <?php
 }
-// +++++Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
+//Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
 else {
-
-  // ++++++TODO: Проверть есть ли такой логин и пароль в базе данных.
-  $login=$_POST['login'];
-  //вход в БД
-  $user='u47586'; $pass='3927785';
-  $db=new PDO('mysql:host=localhost;dbname=u47586',$user,$pass, array(PDO::ATTR_PERSISTENT=>true));
-  //поиск соответствующего логина
-  $sth = $dbh->prepare("SELECT '*' FROM `users` WHERE `login` = ?");
-  $sth->execute(array($login));
-  $value = $sth->fetch(PDO::FETCH_ASSOC);
-  if(empty($value))// нет такого пользователя
-  {
-    echo $value['id']; 
-    //+++++++ Выдать сообщение об ошибках.
-    printf('Пользователь с логином %s не существует!', $login);
-  }
-  else
-  {// ++++Если все ок, то авторизуем пользователя.
-  $_SESSION['login'] = $login;
-  //___________________________??????????Зачем он ???__ Записываем ID пользователя.
-  $_SESSION['uid'] = 123;
-  }
-  // ______________??????___Куда__???___Делаем перенаправление.
-  header('Location: form.php');
-
-//_________________________________SELECT *  
-// $result = $mysqli->query('SELECT * FROM `table_name`'); // запрос на выборку
-// while($row = $result->fetch_assoc())// получаем все строки в цикле по одной
-// {
-//     echo '<p>Запись id='.$row['id'].'. Текст: '.$row['text'].'</p>';// выводим данные
-// }
+  //_____________________________________________Регистрация__________________________________________________________________
+  if (isset($_POST['registration'])) {
+    setcookie('registration', '1', time() + 24 * 60 * 60);
+    header('Location: login.php');
+  } else //__________________________________________Вход__________________________________________________________________________
+    if (isset($_POST['enter'])) {
+      //  Проверть есть ли такой логин и пароль в базе данных.
+      $loginu = $_POST['login'];
+      $passu = $_POST['pass'];
+      //вход в БД
+      $user = 'u47586';
+      $pass = '3927785';
+      $db = new PDO('mysql:host=localhost;dbname=u47586', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+      //поиск соответствующего логина
+      $sth = $db->prepare("SELECT '*' FROM `users` WHERE `login` = ?");
+      $sth->execute(array($loginu));
+      $value = $sth->fetch(PDO::FETCH_ASSOC);
+      if (empty($value)) // нет такого пользователя
+      {
+        //echo $value['id']; Выдать сообщение об ошибках.
+        $msg['notexist'] = '<div style="color:red"> Пользователь с логином ' . $loginu . ' не существует!</div>';
+      } else if ($value['pass'] != $passu) {
+        $msg['wrong'] = '<div style="color:red"> Неверный пароль!</div>';
+      } else { //Если все ок, то авторизуем пользователя.
+        $_SESSION['login'] = $loginu;
+        $_SESSION['uid'] = $value['id'];
+        header('Location: index.php');
+      }
+    }
 }
